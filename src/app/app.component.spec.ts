7eaf2593254dc8e66170af9783d86d345963ce2d
dipesh <dipesh_gandhi9@yahoo.com>
2023-03-10 17:40:45 +0530

    Innitial Comment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'technical-assistant'`, () => {
+  it(`should have as title 'navbar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('technical-assistant');
+    expect(app.title).toEqual('navbar');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('technical-assistant app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('navbar app is running!');
   });
 });
