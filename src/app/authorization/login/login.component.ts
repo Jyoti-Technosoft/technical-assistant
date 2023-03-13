a0341e8323fc699eb3164e687f7ced994322449c
Isha Mistry <isha.jtdev@gmail.com>
2023-03-13 15:19:52 +0530

    TATB-01 :   -made login and registration page responsive

--- src/app/authorization/login/login.component.ts
+++ src/app/authorization/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-
 import { DialogService } from 'src/app/dialog-service/dialog.service';
 
 import dialogData from 'src/assets/json/dialogData.json';
@@ -78,10 +77,6 @@ export class LoginComponent implements OnInit, OnDestroy {
     });
   }
 
-  get adminFormValidator() {
-
-    return this.adminForm.controls;
-  }
   ngOnDestroy(): void {
   }
 
