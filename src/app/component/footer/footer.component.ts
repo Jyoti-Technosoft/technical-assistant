8fd2fe63878873c3723ddc1080525b902b6283bf
prabhat thakur <prabhat.jtdev@gmail.com>
2023-03-16 17:33:25 +0530

    TATB - 32 : Add Footer section for all page

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  icons: { icon: string }[] = [
+    { icon: 'facebook' },
+    { icon: 'twitter' },
+    { icon: 'linkedin' },
+    { icon: 'github' },
+    { icon: 'instagram' },
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
