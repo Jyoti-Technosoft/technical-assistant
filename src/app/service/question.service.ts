6d2decd759f38d6c740815d0e613ef0f894382a4
Isha Mistry <isha.jtdev@gmail.com>
2023-03-20 16:37:51 +0530

    TATB:01 :    -changed css type    - removed css file    - cleanup question service file     -changed import in app service and removed http client module

--- src/app/service/question.service.ts
+++ src/app/service/question.service.ts
@@ -1,20 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuestionService {
-  userName: any;
-  points: number = 0;
-  // correctAnswer:number = 0;
-  correctanswer: number = 0;
-  inCorrectAnswer: number = 0;
   public selectedQuiztype!: string;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
   selectedQuizType!: string;
-  questions() {}
+
   ngOnInit(): void {}
 
   getUser(): string | boolean {
