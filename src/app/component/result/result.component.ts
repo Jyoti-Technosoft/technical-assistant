3596c68bf423fc7574858ef0fa42bbc391239a6e
Prabhat Thakur <prabhat.jtdev@gmail.com>
2023-03-20 12:15:33 +0530

    TATB -35 :Show only recently completed result then create option for showing all results      -Remove all the unnecessary css and repeating css      -Reduce the number of variables by removing unnecessary variables      -Disable show more button if all results are shows      -check if unnecessary code is there and remove it

--- src/app/component/result/result.component.ts
+++ src/app/component/result/result.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { QuestionService } from '../../service/question.service';
-import quizData from 'src/assets/json/data.json';
 
 @Component({
   selector: 'app-result',
@@ -8,15 +7,12 @@ import quizData from 'src/assets/json/data.json';
   styleUrls: ['./result.component.css'],
 })
 export class ResultComponent implements OnInit, OnDestroy {
-  quizData = { ...quizData };
   userName: any;
-  userData: any;
   submittedData: any;
 
   constructor(public questionService: QuestionService) {}
 
   ngOnInit(): void {
-    this.userName = this.questionService?.userName;
     this.resultData();
     if (!this.userName) {
       this.getData();
@@ -25,26 +21,20 @@ export class ResultComponent implements OnInit, OnDestroy {
 
   getData() {
     let data: any = localStorage.getItem('registeruser');
-    this.userData = JSON.parse(data);
-    this.userName = this.userData.find((data:any)=>{
-      return data.id == this.questionService.getUser()
+    this.userName = JSON.parse(data).find((data: any) => {
+      return data.id == this.questionService.getUser();
     })?.fullname;
   }
 
   resultData() {
     let data: any = localStorage.getItem('result');
-    this.submittedData = JSON.parse(data);
-    this.submittedData = this.submittedData.filter((data: any) => {
+    this.submittedData = JSON.parse(data).filter((data: any) => {
       return data?.user == this.questionService.getUser();
     });
-    this.submittedData = this.submittedData.map((data: any) => {
-      const message = this.quizData?.Quiz?.find((quizData: any) => {
-        return quizData.quizId == data.type;
-      });
-      data.image = message?.image;
-      return data;
-    });
-    this.submittedData = this.submittedData.splice(this.submittedData.length-1,1);
+    this.submittedData = this.submittedData.splice(
+      this.submittedData.length - 1,
+      1
+    );
   }
 
   ngOnDestroy(): void {}
