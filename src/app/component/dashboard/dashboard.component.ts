5409ff92aac93611952b655cbd13d992ed902994
Prabhat Thakur <prabhat.jtdev@gmail.com>
2023-03-22 17:58:33 +0530

    - Give proper name for alt in img. -give null check where required -give camelCase names

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import quizData from '../../../assets/json/data.json';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit, OnDestroy {
   quizData = { ...quizData };
