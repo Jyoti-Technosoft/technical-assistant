e6231a73a70ef17660bfe3e27b318788a49db876
Prabhat Thakur <prabhat.jtdev@gmail.com>
2023-04-03 13:00:24 +0530

    TATB-48  : Take pull of stagging all pr branch

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../authorization/auth-guard/auth.guard';
-import { DashboardComponent } from '../component/dashboard/dashboard.component';
-import { Quizcomponent } from '../component/quiz/quiz.component';
-import { ResultComponent } from '../component/result/result.component';
-import { StartquizComponent } from '../component/startquiz/startquiz.component';
-import { FullLayoutComponent } from './full-layout/full-layout.component';
-import { AllresultsComponent } from '../component/allresults/allresults.component';
+import { DashboardComponent } from '@app/component/dashboard/dashboard.component';
+import { Quizcomponent } from '@app/component/quiz/quiz.component';
+import { ResultComponent } from '@app/component/result/result.component';
+import { StartquizComponent } from '@app/component/startquiz/startquiz.component';
+import { FullLayoutComponent } from '@app/layout/full-layout/full-layout.component';
+import { AllresultsComponent } from '@app/component/allresults/allresults.component';
 import { LoginComponent } from '@app/authorization/login/login.component';
 import { RegistrationComponent } from '@app/authorization/registration/registration.component';
 import { NonAuthGuard } from '@app/authorization/auth-guard/non-auth.guard';
+import { PageNotFoundComponent } from '@app/component/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -22,7 +23,8 @@ const routes: Routes = [
       { path: 'quizname', component: StartquizComponent },
       { path: 'quiz', component: Quizcomponent },
       { path: 'result', component: ResultComponent },
-      { path: 'allresults', component: AllresultsComponent }
+      { path: 'allresults', component: AllresultsComponent },
+      { path: '**', redirectTo: '404Page', pathMatch: 'full' },
     ],
   },
   {
@@ -33,12 +35,15 @@ const routes: Routes = [
       { path: '', redirectTo: '/login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent }, // new  module
       { path: 'registration', component: RegistrationComponent }, // new module
+      { path: '**', redirectTo: '404Page', pathMatch: 'full' },
     ],
-  }
+  },
+  { path: '404Page', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '404Page', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class LayoutRoutingModule {}
