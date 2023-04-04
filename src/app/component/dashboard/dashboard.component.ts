32f5a2ce2e26d27c9cd134c6bb5adc6bc3d88c68
Poojan Patel <127502736+PoojanPatelJT@users.noreply.github.com>
2023-04-04 11:46:41 +0530

    Revert "TATB - 49 :  Add Load More Button On Dashboard [ feature/development ]"

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -11,15 +11,14 @@ import { getAllQuiz, selectQuiz } from '@app/store/quiz/quiz.action';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-
 export class DashboardComponent implements OnInit, OnDestroy {
   quizData = { ...quizData };
   destroy$:ReplaySubject<boolean> = new ReplaySubject();
-  quizs: any[] = [];
-  cardData: number = 8;
+  quizs: any;
+  initialData: number = 8;
 
   constructor(private route: Router, private store: Store) {}
- 
+
   ngOnInit(): void {
     this.store
       .select((state: any) => state.quiz)
@@ -38,8 +37,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.route.navigate(['/quizname'], { queryParams });
   }
 
-  loadMore() {
-    this.cardData = this.cardData + 8;
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
-  ngOnDestroy() {}
 }
